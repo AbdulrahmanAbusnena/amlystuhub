e1cb2f9b781fc37a92ec35986d94bb8e251cc5d3
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-16 03:26:18 +0200

    git

--- lib/features/auth/data/services/auth_service.dart
+++ lib/features/auth/data/services/auth_service.dart
@@ -2,4 +2,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // stream to listen to authentication state changes.
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // get the current user
+  User? get currentUser => _auth.currentUser;
+
+  // sign in with email and password
+  Future<UserCredential?> signInWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      // Rethrow the error so your controller can catch it and show it to the student
+      throw _handleAuthException(e);
+    } catch (e) {
+      throw 'An unexpected error occurred. Please try again.';
+    }
+  }
+
+  _handleAuthException(FirebaseAuthException e) {}
 }
