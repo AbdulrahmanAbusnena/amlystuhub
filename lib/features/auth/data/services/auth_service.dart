daafabefe63b00589b4157c00b18fdf237d7b042
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-16 03:13:20 +0200

    added stream

--- lib/features/auth/data/services/auth_service.dart
+++ lib/features/auth/data/services/auth_service.dart
@@ -2,4 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // stream to listen to authentication state changes.
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
 }
