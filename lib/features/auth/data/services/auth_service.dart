0f9596ea4d9f9642a54d1bed1385a98b9a40e20b
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-16 03:14:04 +0200

    getting user

--- lib/features/auth/data/services/auth_service.dart
+++ lib/features/auth/data/services/auth_service.dart
@@ -5,4 +5,7 @@ class AuthService {
 
   // stream to listen to authentication state changes.
   Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // get the current user
+  User? get currentUser => _auth.currentUser;
 }
