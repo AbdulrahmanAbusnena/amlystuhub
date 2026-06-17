24bec5fb5be819af5b5f80c2e3cfedb24eac0a94
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-17 04:21:25 +0200

    completed the auth states, I'm going to add the providers next

--- lib/features/auth/presentation /controllers/auth_controllers.dart
+++ lib/features/auth/presentation /controllers/auth_controllers.dart
@@ -1,1 +1,16 @@
-enum AuthStatus { authenticated, unauthenticated, unknown }
+enum AuthStatus { initial, authenticated, unauthenticated, unknown }
+
+class AuthState {
+  final AuthStatus status;
+  final String? errorMessage;
+
+  AuthState({required this.status, this.errorMessage});
+
+  // Factory constructor to make changing the state easier (learned this from Fireship)
+  factory AuthState.initial() => AuthState(status: AuthStatus.initial);
+  factory AuthState.authenticated() =>
+      AuthState(status: AuthStatus.authenticated);
+  factory AuthState.unauthenticated() =>
+      AuthState(status: AuthStatus.unauthenticated);
+  factory AuthState.unknown() => AuthState(status: AuthStatus.unknown);
+}
