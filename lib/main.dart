7f7793f12bd988d809891a4cf039939171bc3245
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-20 23:23:28 +0200

    done with the theme controller and importing the logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:amlystuhub/authgate.dart';
+import 'package:amlystuhub/core/theme/theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'firebase_options.dart';
+
+// Expose your theme class to the Riverpod engine cleanly
+final appThemeProvider = ChangeNotifierProvider<ThemeProvider>((ref) {
+  return ThemeProvider();
+});
 
 void main() async {
   // initiating firebase
@@ -10,17 +17,22 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the theme using Riverpod's ref pointer
+    final themeProviderState = ref.watch(appThemeProvider);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Amly Student Hub',
-      theme: ThemeData(),
+      theme: themeProviderState
+          .themeData, // Pulls the active raw ThemeData palette
       home: AuthGateKeeper(),
     );
   }
