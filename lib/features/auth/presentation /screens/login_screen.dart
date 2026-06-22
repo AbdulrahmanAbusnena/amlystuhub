a264445f34ce666a6a9e3ba003d1753bc33e7a0b
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-22 04:12:51 +0200

    added auth setup logic

--- lib/features/auth/presentation /screens/login_screen.dart
+++ lib/features/auth/presentation /screens/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:amlystuhub/core/widgets/retro_window_shell.dart';
+import 'package:amlystuhub/features/auth/presentation%20/controllers/auth_controllers.dart';
+import 'package:amlystuhub/features/dashboard/presentation/screens%20/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -23,6 +26,26 @@ class _LoginState extends ConsumerState<Login> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return const Placeholder();
+    // auth controller setup
+    final authState = ref.watch(authStateProvider);
+    final isLoading = authState.status == AuthState.loading();
+
+    ref.listen<AuthState>(authStateProvider, (previous, next) {
+      if (next.status == AuthStatus.error && next.errorMessage != null) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(next.errorMessage!)));
+        ref.read(authStateProvider.notifier).resetState();
+      } else if (next.status == AuthStatus.authenticated) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('WELCOME BACK!.')));
+        Navigator.of(
+          context,
+        ).pushReplacement(MaterialPageRoute(builder: (_) => const Dashboard()));
+      }
+    });
+
+    return const RetroWindowShell();
   }
 }
