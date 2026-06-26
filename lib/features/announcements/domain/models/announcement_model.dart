c205710805f1d82b56e9d2fe7c596dc2e7a25402
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-26 19:12:32 +0200

    added necessary strings

--- lib/features/announcements/domain/models/announcement_model.dart
+++ lib/features/announcements/domain/models/announcement_model.dart
@@ -1,1 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AnnouncementsModel {
+  final String id;
+  final String title;
+  final String content;
+  final String authorId;
+  final String authorName;
+  final String authRole; // Role: 'head_of_academics' or 'school_admin
+  final String category; // 'AP' 'General' 'Grade 9'
+
+  final List<int> targetGrades; // e.g., [10, 11] to restrict visibility
+  final bool apOnly;
+
+  final DateTime createdAt;
+  final List<String> pinnedByUids;
+}
