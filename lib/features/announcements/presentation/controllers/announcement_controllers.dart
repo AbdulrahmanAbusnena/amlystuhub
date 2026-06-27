efb7463e9bf542259dcc6e65595adb6606b1f0a7
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-27 09:38:45 +0200

    created initialization for our variables

--- lib/features/announcements/presentation/controllers/announcement_controllers.dart
+++ lib/features/announcements/presentation/controllers/announcement_controllers.dart
@@ -3,4 +3,6 @@ enum AnnouncementControllers { initial, loading, success, error }
 class AnnouncementState {
   final AnnouncementState status;
   final String? errorMessage;
+
+  AnnouncementState({required this.status, this.errorMessage});
 }
