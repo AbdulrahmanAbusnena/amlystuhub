b666d23e984aea81840dece8128733c7dcb9d497
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-27 11:18:13 +0200

    added method

--- lib/features/auth/domain/models /user_role.dart
+++ lib/features/auth/domain/models /user_role.dart
@@ -1,1 +1,20 @@
-enum UserRole { student, stuCoLead, headOfAcademics, stuCoAdmin }
+enum UserRole {
+  student,
+  stuCoLead,
+  headOfAcademics,
+  stuCoAdmin;
+
+  static UserRole fromString(String roleStr) {
+    switch (roleStr) {
+      case 'stuco_leads':
+        return UserRole.stuCoLead; // Updated lookup
+      case 'head_of_academics':
+        return UserRole.headOfAcademics;
+      case 'school_admin':
+        return UserRole.stuCoAdmin;
+      case 'student':
+      default:
+        return UserRole.student;
+    }
+  }
+}
