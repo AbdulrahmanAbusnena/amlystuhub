b42c30fffe0d23aff76ba22683201453e46b3745
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-27 09:44:49 +0200

    added status to the state

--- lib/features/announcements/presentation/controllers/announcement_controllers.dart
+++ lib/features/announcements/presentation/controllers/announcement_controllers.dart
@@ -1,8 +1,22 @@
-enum AnnouncementControllers { initial, loading, success, error }
+enum AnnouncementStatus { initial, loading, success, error }
 
 class AnnouncementState {
-  final AnnouncementState status;
+  final AnnouncementStatus status;
   final String? errorMessage;
 
   AnnouncementState({required this.status, this.errorMessage});
+
+  factory AnnouncementState.initial() =>
+      AnnouncementState(status: AnnouncementStatus.initial);
+
+  factory AnnouncementState.loading() =>
+      AnnouncementState(status: AnnouncementStatus.loading);
+
+  factory AnnouncementState.success() =>
+      AnnouncementState(status: AnnouncementStatus.success);
+
+  factory AnnouncementState.error(String message) => AnnouncementState(
+    status: AnnouncementStatus.error,
+    errorMessage: message,
+  );
 }
