099360f015db3ef2f7752a8732928c7582c61807
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-29 16:31:27 +0200

    adding isntances to announcemnt_controllers

--- lib/features/announcements/presentation/controllers/announcement_controllers.dart
+++ lib/features/announcements/presentation/controllers/announcement_controllers.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/models/announcement_model.dart';
 import 'announcement_state.dart';
-import 'package:amlystuhub/features/auth/domain/models /user_model.dart';
+import 'package:amlystuhub/features/auth/domain/models/user_model.dart';
+import '../../data/announcement_services.dart';
+
+class AnnouncementController extends StateNotifier<AnnouncementState> {
+  late final AnnouncementServices _service;
+}
