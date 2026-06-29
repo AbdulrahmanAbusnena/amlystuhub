bd0a45a76246af7d5fa8bcc0c934414740779cb0
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-29 05:58:48 +0200

    created controllers and added file imports

---
+++ lib/features/announcements/presentation/controllers/announcement_controller.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../domain/models/announcement_model.dart';
+import 'announcement_state.dart';
+import 'package:amlystuhub/features/auth/domain/models /user_model.dart';
