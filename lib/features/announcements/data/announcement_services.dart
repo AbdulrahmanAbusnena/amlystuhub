6f61becc05d12d84eff0558a68de535e9acaf95f
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-06-29 15:38:07 +0200

    just found structural floww within my announcement services

--- lib/features/announcements/data/announcement_services.dart
+++ lib/features/announcements/data/announcement_services.dart
@@ -4,7 +4,18 @@ import '../domain/models/announcement_model.dart';
 class AnnouncementServices {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // fetching the Stream
+  // Planning on creating server side filtering
+
+  /*  
+
+ So after doing research, and reviews done by Gemini and ChatGPT I found a 
+ structural error, I found the query fetches every single announcement ever 
+ created in the entire school database. For example if the school hits 1,000
+ announcements, a grade 9 students will have to download 1,000 documents over the 
+ networks just to read 3 updates relevant to them. It will make the reads skyrockets 
+ and the rendering process, highly inefficient. 
+  */
+  //  // fetching the Stream
   Stream<List<AnnouncementsModel>> getAnnouncementsStream() {
     return _firestore
         .collection('announcements')
