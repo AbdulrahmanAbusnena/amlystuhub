6bf1d3907ad43fbad6a11cecc77f3e32fb823b47
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-07-01 14:14:39 +0200

    deleted a comment

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,7 @@
   const admin = require("firebase-admin"); 
   const nodemailer = require("nodemailer");
   
- // * See a full list of supported triggers at https://firebase.google.com/docs/functions
-
+ 
  
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
