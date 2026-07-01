91293e91b8a8a494d3030e7bcf9c17eb3b9de270
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-07-01 16:46:02 +0200

    pointed out the plan for the functions

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,24 @@
   
   admin.initializeApp();
   const db = admin.firestore(); 
+   
+ // configuring the connection to our official StuCo account 
+
+ // cloud function trigger listening to our announcements tigger in firestore 
+
+ // constructing the targeted user query based on announcement criteria
   
+ // filtering out students who aren't in the designated target grades array 
+
+ // filter out non-ap students if the flag is checked as true 
+
+ // Guard clause if no students match this specific demographic profile 
+
+ // the structural formatting layout for the student emails  
  
+ // sneding the emails to networks stream 
+
+
  
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
