111d8c8a8c39c4bde721aa72c8ff80d8da53a728
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-07-01 21:44:10 +0200

    initialized class

--- lib/features/announcements/presentation/controllers/email_controller.dart
+++ lib/features/announcements/presentation/controllers/email_controller.dart
@@ -1,3 +1,16 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+Future<void> sendAnnouncementEmail({
+ required String title; 
+ required String content; 
+ required String category; 
+ required String recipienEmai; 
+ required String authorRole,
+  required List<String> targetGrades,
+  required bool apOnly, 
+
+  
+}); 
