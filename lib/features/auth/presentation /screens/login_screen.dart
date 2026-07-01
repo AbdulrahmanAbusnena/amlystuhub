ea1a4bc43ad7848630a4632c38f690daf936fe0a
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-07-01 10:49:57 +0200

    added necessary imorts and methods to login

--- lib/features/auth/presentation /screens/login_screen.dart
+++ lib/features/auth/presentation /screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amlystuhub/core/widgets/retro_window_shell.dart';
 import 'package:amlystuhub/features/auth/presentation%20/controllers/auth_controllers.dart';
+import 'package:amlystuhub/features/auth/presentation%20/providers/auth_providers.dart';
 import 'package:amlystuhub/features/dashboard/presentation/screens%20/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
