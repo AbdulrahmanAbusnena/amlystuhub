def41bc5ed995d0324b370ddaac540724aaaf6ce
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-08-03 11:53:43 +0200

    done with the announcement state

---
+++ lib/announcements/presentation/state/announcement_state.dart
@@ -0,0 +1,19 @@
+enum AnnouncementStatus { initial, loading, success, error }
+
+class AnnouncementState {
+  final AnnouncementStatus status;
+  final String? errorMessage;
+
+  AnnouncementState({required this.status, this.errorMessage});
+
+  factory AnnouncementState.initial() =>
+      AnnouncementState(status: AnnouncementStatus.initial);
+  factory AnnouncementState.loading() =>
+      AnnouncementState(status: AnnouncementStatus.loading);
+  factory AnnouncementState.success() =>
+      AnnouncementState(status: AnnouncementStatus.success);
+  factory AnnouncementState.error(String message) => AnnouncementState(
+    status: AnnouncementStatus.error,
+    errorMessage: message,
+  );
+}
