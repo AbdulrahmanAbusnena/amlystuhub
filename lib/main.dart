c907737bd85cdeed344715ea570bcafced8c992f
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-08-11 19:30:09 +0200

    done with adding themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:amlystuhub/core/router/go_router.dart';
-import 'package:amlystuhub/core/theme/theme_controller.dart';
+import 'package:amlystuhub/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import 'firebase_options.dart';
 
-// Expose your theme class to the Riverpod engine cleanly
-final appThemeProvider = ChangeNotifierProvider<ThemeProvider>((ref) {
-  return ThemeProvider();
-});
-
 void main() async {
-  // initiating firebase
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -25,13 +18,13 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeProviderState = ref.watch(appThemeProvider);
+    final activeTheme = ref.watch(currentThemeProvider);
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'AmlyStuHub',
-      theme: themeProviderState.themeData,
+      theme: activeTheme,
       routerConfig: router,
     );
   }
