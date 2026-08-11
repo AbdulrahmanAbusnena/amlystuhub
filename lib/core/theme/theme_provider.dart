7886110cbc78dd5aa96ea7aab9ceecc31e742138
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-08-11 22:52:31 +0200

    adjusted the themes

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -3,21 +3,45 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'app_themes.dart';
 
-class ThemeNotifier extends StateNotifier<AppThemeKey> {
-  ThemeNotifier() : super(AppThemeKey.darkSlate);
+class ThemeState {
+  final AppThemeKey themeKey;
+  final AppFontFamily fontFamily;
 
-  void setTheme(AppThemeKey key) {
-    state = key;
+  const ThemeState({required this.themeKey, required this.fontFamily});
+
+  ThemeState copyWith({AppThemeKey? themeKey, AppFontFamily? fontFamily}) {
+    return ThemeState(
+      themeKey: themeKey ?? this.themeKey,
+      fontFamily: fontFamily ?? this.fontFamily,
+    );
   }
 }
 
-final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppThemeKey>(
-  (ref) {
-    return ThemeNotifier();
-  },
-);
+class ThemeNotifier extends StateNotifier<ThemeState> {
+  ThemeNotifier()
+    : super(
+        const ThemeState(
+          themeKey: AppThemeKey.lightStandard,
+          fontFamily: AppFontFamily.standard,
+        ),
+      );
+
+  void setTheme(AppThemeKey key) {
+    state = state.copyWith(themeKey: key);
+  }
+
+  void setFont(AppFontFamily font) {
+    state = state.copyWith(fontFamily: font);
+  }
+}
+
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, ThemeState>((
+  ref,
+) {
+  return ThemeNotifier();
+});
 
 final currentThemeProvider = Provider<ThemeData>((ref) {
-  final activeKey = ref.watch(themeNotifierProvider);
-  return AppThemes.getTheme(activeKey);
+  final state = ref.watch(themeNotifierProvider);
+  return AppThemes.getTheme(state.themeKey, state.fontFamily);
 });
